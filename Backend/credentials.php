65d5ed43c5cf7395d367716c57a0d4fa95932183
rolanntig <106159101+rolanntig@users.noreply.github.com>
2023-11-13 10:40:51 +0100

    ff

--- Backend/credentials.php
+++ Backend/credentials.php
@@ -1,9 +1,9 @@
 <?php
 
 // this file will contain the credentials for the database
-$server = "****";
-$username = "****";
-$password = "****";
+$server = "mysql.jawad.se";
+$username = "kassa";
+$password = " cGZZ2.I2mYPE*T@p";
 $dbname = "kassa";
 $port = 80;
 
