74ebba61491c24a4d154a549b5109242b7fe829f
Tayfun <ripeaglee@hotmail.com>
2023-11-13 16:02:50 +0100

    Removed php from front- to backend
    Removed php code for product register and view from the front end to backend

--- Backend/Register_Product.php
+++ Backend/Register_Product.php
@@ -1,21 +1,38 @@
 <?php 
-    include "../Backend/credentials.php";
-    include "../Backend/databaseHandler.php";
-    include "../Backend/console.php";
-    $conn = mysqli_connect($server, $username, $password, $dbname, $port);
-    function register($product_name,$product_info,$expire_date,$pris,$barcode,$antal){
-        global $conn;
-        $command = 
-            "INSERT INTO `Products` (`ID`, `product_name`, `product_info`, `expire_date`, `price`, `barcode`, `amount`)
-                VALUES ('$product_name', '$product_info', '$expire_date', '$pris', '$barcode', '$antal') /*ON DUPLICATE KEY UPDATE (amount='$antal', price='$pris'*/;";  
-        
-        try {
-            $conn->query($command) or exit(console("DUPLICATE: "($conn->error)));
-            console("Registration was successful");  
-        
-        } catch (\Throwable $th) {
-            console($th->getMessage());
-            console($th->getLine());
-        } 
-    }
+    include "./Backend/credentials.php";
+    $sql = "INSERT INTO Products (product_name, product_info, expire_date, price, barcode,amount,category) 
+                VALUES ('$product', '-', '$expire', '$price', '$barcode','$amount','-')";
+            try {
+                if ($conn->query($sql)) {
+                    //echo "<script>alert('Produkt registrerad')</script>";
+                    echo "<div class='card border-0 shadow rounded-3 br1em mt-2 text-center bg-success text-white w-50 mx-auto align-middle'>
+                                <p class='p-2 pt-4'>
+                                    Produkt registrerad
+                                </p>
+                            </div>
+                            ";
+                                header("Refresh:2; url=#");
+                            } else {
+                                echo "<div class='card border-0 shadow rounded-3 br1em mt-2 text-center bg-danger text-white w-50 mx-auto align-middle'>
+                                        <p class='p-2 pt-4'>
+                                            Registrering misslyckades
+                                        </p>
+                                    </div>
+                                    ";
+                                    
+                                $dom = new DOMDocument();
+                                $dom->loadHTML($html);
+                                $xpath = new DOMXPath($dom);
+                                $hrefs = $xpath->evaluate("/html/body/div/div/form/div/input");
+
+                                // console($hrefs);
+
+                                header("Refresh:10; url=#");
+                                }
+                            }
+                        catch (\Throwable $th) {
+                            console($th->getMessage());
+                            console($th->getLine());
+                        }
+
 ?>
--- Frontend/produkter.php
+++ Frontend/produkter.php
@@ -40,85 +40,21 @@
                         include "../Backend/credentials.php";
                                             
                         //Registering a product
-                        if(isset($_POST['submit'])){
-                            $product    = $_POST['product'];
-                            $amount     = $_POST['amount'];
-                            $price      = $_POST['price'];
-                            $expire     = $_POST['expire'];
-                            $barcode    = $_POST['barcode'];
-
-                            $sqlquery = "INSERT INTO Products (product_name, product_info, expire_date, price, barcode,amount,category) 
-                            VALUES ('$product', '-', '$expire', '$price', '$barcode','$amount','-')";
-                            try {
-                                if ($conn->query($sqlquery)) {
-                                    //echo "<script>alert('Produkt registrerad')</script>";
-                                    echo "<div class='card border-0 shadow rounded-3 br1em mt-2 text-center bg-success text-white w-50 mx-auto align-middle'>
-                                            <p class='p-2 pt-4'>
-                                                Produkt registrerad
-                                            </p>
-                                        </div>
-                                    ";
-                                    header("Refresh:2; url=#");
-                                }
-                                else {
-                                    echo "<div class='card border-0 shadow rounded-3 br1em mt-2 text-center bg-danger text-white w-50 mx-auto align-middle'>
-                                            <p class='p-2 pt-4'>
-                                                Registrering misslyckades
-                                            </p>
-                                        </div>
-                                    ";
-                                    
-                                    $dom = new DOMDocument();
-                                    $dom->loadHTML($html);
-                                    $xpath = new DOMXPath($dom);
-                                    $hrefs = $xpath->evaluate("/html/body/div/div/form/div/input");
-
-                                    console($hrefs);
-
-                                    header("Refresh:10; url=#");
-                                }
-                            }
-                            catch (\Throwable $th) {
-                                console($th->getMessage());
-                                console($th->getLine());
-                            }
+                        $product    = $_POST['product'];
+                        $amount     = $_POST['amount'];
+                        $price      = $_POST['price'];
+                        $expire     = $_POST['expire'];
+                        $barcode    = $_POST['barcode'];
+                        if (isset($_POST['submit'])){
+                            include '../Backend/Register_Product.php';
                         }
                     ?>
                 </div>
             </form>
             <h1 class="mb-4 mt-3 text-center">Produkter</h1>
-            <table id="produkterKvar" class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Produkt</th>
-                        <th scope="col">Antal</th>
-                        <th scope="col">Pris</th>
-                        <th scope="col">Bäst före</th>
-                        <th scope="col">Barcode</th>
-                    </tr>
-                </thead>
-                <?php
-                    try {
-                        $sqlGetProduct = 'SELECT * FROM Products';
-                        $products= mysqli_query($conn, $sqlGetProduct);
-                        //fetches all products from database
-                        while($row = mysqli_fetch_assoc($products)){
-                            echo '<tr>';
-                            echo '<td>'.$row['product_name'].'</td>';
-                            echo '<td>'.$row['amount'].'</td>';
-                            echo '<td>'.$row['price'].' kr </td>';
-                            echo '<td>'.$row['expire_date'].'</td>';
-                            echo '<td>'.$row['barcode'].'</td>';
-                            echo '</tr>';
-                        }
-                        
-                        mysqli_close($conn);
-                    } catch (\Throwable $th) {
-                        console($th->getMessage());
-                        console($th->getLine());
-                    }
-                ?>
-            </table>
+            <?php 
+            include '../Backend/Product_view';
+            ?>
         </div>
     </div>
 </body>
