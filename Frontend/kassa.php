e2b4fce3cddda94bfc75e284c2c201603a3c3d0d
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-13 14:16:59 +0100

    itemContainer and sample items, some styling

--- Frontend/kassa.php
+++ Frontend/kassa.php
@@ -7,13 +7,35 @@
 	<link rel="stylesheet" href="./Styles/kassa.css">
 </head>
 <body>
+	<!-- add header -->
+	<!-- container -->
 	<div class="fullDiv">
+		<!-- left container -->
 		<div class="leftDiv">
-			<form action="/action_page.php">
-				<input type="text" id="barcode" name="barcode" ><br><br>
+			<!-- container where items show -->
+			<div class="itemContainer">
+				<!-- barcode + item form -->
+			<form action="#" class="barcodeForm">
+				<input type="text" id="barcode" name="barcode" >
 				<input type="submit" value="Submit">
 			</form>
+			<!-- sample pictures REMOVE LATER -->
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+				<a href="#"><img src="https://static.openproductsfacts.org/images/products/505/189/323/8136/front_es.3.full.jpg" alt="fortnint" width="150"></a>
+			</div>
 		</div>
+		<!-- right container -->
 		<div class="rightDiv">
 
 		</div>
