156d1c95e3a3e9bb5b868d44f8adcf21a6b54d6e
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-11-14 09:34:34 +0100

    selectCategory.js update
    added comments

--- Scripts/selectCategory.js
+++ Scripts/selectCategory.js
@@ -1,11 +1,14 @@
-const div = document.querySelector(".itemContainer")
+// select container in html code with the following class
+const div = document.querySelector(".itemContainer");
+// select the "select" tag with the following id
 let selectElement = document.getElementById("category");
 
+// function that changes the id of the container
 function handleCategoryChange() {
     var selectedOption = selectElement.options[selectElement.selectedIndex];
     var selectedValue = selectedOption.value;
     div.id = selectedValue;
-    console.log(selectedValue);
 }
 
+// calls the function above when the select tag is changed
 selectElement.addEventListener("change", handleCategoryChange);
