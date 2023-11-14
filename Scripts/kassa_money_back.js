54fcfef415040cb6121a941c9d32aeb7e114c992
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-14 14:58:45 +0100

    Hehe

--- Scripts/kassa_money_back.js
+++ Scripts/kassa_money_back.js
@@ -1,3 +1,2 @@
 let money = document.getElementById("payed");
-let newMoney = money.slice(money.search(" "));
-let newMoneyInt= Number(newMoney.slice(newMoney.search("4"),newMoney.search("k")));
+let newMoneyInt=Number((money.slice(money.search(" "),money.search("kr"))));
