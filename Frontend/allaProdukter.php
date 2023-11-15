22352899e02123f1b5b6e82f127e9f2dc5222699
Tayfun <ripeaglee@hotmail.com>
2023-11-15 09:27:13 +0100

    search and filter works again

--- Frontend/allaProdukter.php
+++ Frontend/allaProdukter.php
@@ -11,49 +11,47 @@
     <title>Produkter</title>
 </head>
 <body> 
-<?php include "../Backend/header.php" ?>
-<div class="container">
-<div class="bg-white p-3 br1em">
-<?php include "../Backend/produktNav.php" ?>    
-<h1 class="text-center">Produkter</h1>
-<form method="post" enctype="multipart/form-data" class="d-flex justify-content-center w-100"class="my-4 p-4 border">
-<div class="form-group col-8">
-            <div>
-    <div class="row">
-        <div class="col-md-6 offset-md-3 mt-3 p-2 g-col-6">
-            <form action="" method="GET" class="form-inline">
-                <div class="input-group">
-                    <div class="input-group mb-3">
-                    <input type="text" name="search" class="form-control" placeholder="Search for products">
+    <?php include "../Backend/header.php" ?>
+    <div class="container">
+        <div class="bg-white p-3 br1em">
+            <?php include "../Backend/produktNav.php" ?>    
+            <h1 class="text-center">Produkter</h1>
+            <form method="get" enctype="multipart/form-data" class="d-flex justify-content-center w-100 my-4 p-4 border">
+                <div class="form-group col-8">
+                    <div class="row">
+                        <div class="col-md-6 offset-md-3 mt-3 p-2 g-col-6">
+                            <div class="input-group">
+                                <input type="text" name="search" class="form-control" placeholder="Search for products">
+                                <!-- Dropdown for filtering -->
+                                <select name="filter" class="custom-select">
+                                    <option value="" disabled selected>filter</option>
+                                    <option value="drink">Dricka</option>
+                                    <option value="snacks">Snacks</option>
+                                    <option value="food">Mat</option>
+                                    <option value="verktyg">Verktyg</option>
+                                </select>
+                                <!--Filter button -->
+                                <button type="submit" class="btn btn-outline-secondary"><i class="fas fa-filter"></i> Filter</button>
+                                <!--Reset button -->
+                                <button type="button" class="btn btn-outline-secondary" onclick="resetForm()"><i class="fas fa-sync"></i> Reset</button>
+                            </div>
+                        </div>
                     </div>
-                    <!-- Dropdown for filtering -->
-                    <select name="filter" class="custom-select">
-                        <option value="" disabled selected>filter</option>
-                        <option value="drink">Dricka</option>
-                        <option value="snacks">Snacks</option>
-                        <option value="food">Mat</option>
-                        <option value="verktyg">Verktyg</option>
-                    </select>
-                    <!--Filter button -->
-                    <button type="submit" class="btn btn-outline-secondary  "><i class="fas fa-filter"></i> Filter</button>
-                    <!--Reset button -->
-                    <button type="button" class="btn btn-outline-secondary" onclick="resetForm()"><i class="fas fa-sync"></i>Reset</button>
                 </div>
             </form>
-        </div>
-    </div>
-</div> 
-        </form>
-<script>
-    // JavaScript function to reset the form
-    function resetForm() {
-        document.querySelector('form').reset();
-        // Reset the URL to remove query parameters
-        window.location.href = window.location.pathname; 
-    }
-</script>
-<?php 
-            //includes the php code to view the products
+            <script>
+                // JavaScript function to reset the form
+                function resetForm() {
+                    document.querySelector('form').reset();
+                    // Reset the URL to remove query parameters
+                    window.location.href = window.location.pathname; 
+                }
+            </script>
+            <?php 
+                //includes the php code to view the products
                 include '../Backend/Product_view.php';
             ?>
+        </div>
+    </div>
 </body>
+</html>
