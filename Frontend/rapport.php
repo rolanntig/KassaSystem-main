ad4a025e8b4070b417724c07720b0be4150e6e3c
WixdomCoding <106668839+WixdomCoding@users.noreply.github.com>
2023-11-15 09:58:47 +0100

    Removed navbar.css, improved main.css

--- Frontend/rapport.php
+++ Frontend/rapport.php
@@ -13,9 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./Styles/navbar.css">
     <!-- <link rel="stylesheet" type="text/css" href="./Styles/kassa.css"> -->
     <link rel="stylesheet" type="text/css" href="./Styles/rapport.css">
+    <link rel="stylesheet" href="./Styles/main.css">
     <title>Rapport</title>
 </head>
 
