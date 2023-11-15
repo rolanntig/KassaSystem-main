b049460b8c1f05ee8f4151e29d5add1de18679b0
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-11-15 11:05:08 +0100

    idk

--- Frontend/kassa.php
+++ Frontend/kassa.php
@@ -106,7 +106,7 @@
 		</div>
 	</div>
 	<script>var p = false;</script>
-	<script src="../Scripts/kassa_select_item"></script>
+	<script src="../Scripts/kassa_select_item.js"></script>
 	<script src="../Scripts/kassa_radioHide.js"></script>
 </body>
 </html>
