ad4c90687a06faaad849c777cdd4a4ca48ba13a6
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2023-11-15 13:53:20 +0100

    kassa_backend test filer

--- Backend/kassa_backend.php
+++ Backend/kassa_backend.php
@@ -6,6 +6,9 @@
     if(!isset($_SESSION['cash']) && !is_array($_SESSION['cash'])){
         $_SESSION['cash'] = array();
     }
+    if(!isset($_SESSION['test']) && !is_array($_SESSION['test'])){
+        $_SESSION['test'] = array();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -44,51 +47,37 @@ $data = $result->fetch_all(MYSQLI_ASSOC);
     <!-- Ends the foreach -->
     <?php endforeach ?>
     </div>
+</form>
     <br>
     <div class="d-flex flex-row">
     <?php 
         $id = $_POST['item'];
+
         if ( isset($_POST['item'])){
+
             $result = mysqli_query($conn,"SELECT * FROM Products WHERE `barcode`='$id'");
             $data = $result->fetch_all(MYSQLI_ASSOC);
+
+
             array_push($_SESSION['cart'],$data[0]['product_name']);
             array_push($_SESSION['cash'],$data[0]['price']);
 
             $finalPrice = 0;
-            $endCart = array_count_values($_SESSION['cart']); 
-            $endPrice = array_count_values($_SESSION['cash']);
+            
 
-            echo '<div class="p-2">';
-            echo '<table class="table">';
-            foreach($endCart as $key => $val){
-                echo '<tr>';
-                echo '<td>';
-                echo $key;
-                echo '</td>';
-                echo '</tr>';
-            }
-            echo '</table>';
-            echo '</div>';
-            echo '<div class="p-2">';
-            echo '<table class="table">';
-            foreach($endPrice as $key => $val){
-                echo '<tr>';
-                echo '<td>';
-                $finalPrice += $key * $val;
-                echo $key * $val;
-                echo '</td>';
-                echo '</tr>';
-            }
-            echo '</table>';
-            echo '</div>';
+            include 'kassa_backend_2.php';
+        }
+        if(isset($_POST['rm-item'])){
+            $_SESSION['cart'] = array_filter($_SESSION['cart'], function($v) { return $v != $_POST['rm-item']; });
+            include 'kassa_backend_2.php';
         }
     ?>
     </table>
     </div>
     <form action="" method="POST">
-    <button name="close">
-        Empty Cart
-    </button>
+        <button name="close">
+            Empty Cart
+        </button>
     </form>
     <?php 
         if(isset($_POST['close'])){
@@ -97,6 +86,5 @@ $data = $result->fetch_all(MYSQLI_ASSOC);
             print_r($_SESSION['cart']);
         }
     ?>
-    </form>
 </body>
 </html>
