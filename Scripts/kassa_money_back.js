4a90a2bdf10335dc2af2ca2e04667d3c8f7a97e6
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-11-16 13:45:19 +0100

    12dw

--- Scripts/kassa_money_back.js
+++ Scripts/kassa_money_back.js
@@ -10,33 +10,35 @@ let bills = document.getElementById("bills");
 // funktion som sätter en string i paragrafen (payed)
 function moneyBack() {
   let växel = payed.value - price;
-  let moneyType = [1,5,10,20,50,100,200,500];
+  let moneyType = [1,5,10,20,50,100,200,100000000000000000000000];
   if (price <= payed.value) {
     payBack.textContent = "Växel: " + växel + "kr";
-    bills.textContent = Math.floor(växel/200) + "*200kr " + Math.floor((växel%200)/100) + "*100kr " + 
-    Math.floor((växel%100)/50) + "*50kr " + Math.floor((växel%50)/20) + "*20kr " + 
-    Math.floor((växel%20)/10) + "*10kr " + Math.floor((växel%10)/5) + "*5kr "
-    + " + " +växel % 5 + "kr";
+    // bills.textContent = Math.floor(växel/200) + "200kr " + Math.floor((växel%200)/100) + "100kr " + 
+    // Math.floor((växel%100)/50) + "50kr " + Math.floor((växel%50)/20) + "20kr " + 
+    // Math.floor((växel%20)/10) + "10kr " + Math.floor((växel%10)/5) + "5kr "
+    // + " + " +växel % 5 + "kr";
 
     for(i=0;i <= moneyType.length;i++){
 
-      document.getElementById(`kr${moneyType[i]}`).textContent=Math.floor((växel%moneyType[i+1])/moneyType[i]);
-     
+      document.getElementById(kr${moneyType[i]}).textContent=Math.floor((växel%moneyType[i+1])/moneyType[i]);
+
     }
-    
+
   } else {
     payBack.textContent = "KILL YOURSELF NOW";
+
+    //makes empty string for = 0
     for(i=0;i <= moneyType.length;i++){
-      document.getElementById(`kr${moneyType[i]}`).textContent="";
+      document.getElementById(kr${moneyType[i]}).textContent="";
     }
-    
+
   }
-  if (document.getElementById(`kr${moneyType[i]}`)==0){
-    document.getElementById(`kr${moneyType[i]}`).textContent="";
+
+  if (document.getElementById(kr${moneyType[i]})==0){
+    document.getElementById(kr${moneyType[i]}).textContent="";
   }
 
 }
 
 // när inputden förändras så kallar den funktionen ovanför
 payed.addEventListener("input", moneyBack);
-
