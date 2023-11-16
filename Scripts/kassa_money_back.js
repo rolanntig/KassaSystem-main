77305922693f9e43868c4a26a04b583b186bfa31
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-16 15:29:09 +0100

    Comment before day ends
    :lemon: : :car: :checkered_flag:

--- Scripts/kassa_money_back.js
+++ Scripts/kassa_money_back.js
@@ -16,7 +16,8 @@ function moneyBack() {
   let moneyType = [1,5,10,20,50,100,200,Math.pow(10,1000)];
   // lagar alla errors(ultimate fix)
   try {
-  /* */
+  /* takes modulus of previous bill on current bill, then divides it and floors it to see the amount, and continues untill all money 
+  has gone throught the processs*/
     if (price <= payed.value) {
       payBack.textContent = "Växel: " + växel + "kr";
   
