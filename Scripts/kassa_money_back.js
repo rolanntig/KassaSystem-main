84c3134679dfa33945ce03650f56ee7210d0b5cc
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-16 10:02:16 +0100

    Payback js
    :D

--- Scripts/kassa_money_back.js
+++ Scripts/kassa_money_back.js
@@ -1,2 +1,14 @@
-let money = document.getElementById("payed");
-let newMoneyInt=Number((money.slice(money.search(" "),money.search("kr"))));
+let cost = document.getElementById("cost");
+let newCostInt = Number((cost.slice(cost.search(" "),cost.search("kr"))));
+
+let payed = document.getElementById("payed");
+let newPayedInt=Number((payed.slice(payed.search(" "),payed.search("kr"))));
+
+let payBack= document.getElementById("payBack");
+
+const calculate = ()=>{
+
+  payBack = (cost-payed);
+} 
+
+payed.addEventListener(oninput, ()=> calculate());
