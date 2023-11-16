a9897bb42d0104b0517b6347ce5c731f54014021
Greed <106820018+maxberglund2@users.noreply.github.com>
2023-11-16 13:03:17 +0100

    comments in cart.php

--- Backend/kassa_cart.php
+++ Backend/kassa_cart.php
@@ -1,13 +1,15 @@
 <?php
+// tar allt (*) från databasen
 $result = mysqli_query($conn,"SELECT * FROM Products WHERE `barcode`='$id'");
 $data = $result->fetch_all(MYSQLI_ASSOC);
 
-
+// sätter datan från databasen in i arrayn
 array_push($_SESSION['cart'],$data[0]['product_name']);
 array_push($_SESSION['cash'],$data[0]['price']);
 
+// skapar en variabel för totala summan
 $finalPrice = 0;
 
-
+// inkulderar en anna php fil
 include '../Backend/kassa_backend.php';
 ?>
