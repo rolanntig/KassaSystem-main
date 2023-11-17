c1af93d7a7d95f53b18287f20ca2107ee1b40ed1
rolanntig <106159101+rolanntig@users.noreply.github.com>
2023-11-17 10:17:17 +0100

    session

--- Frontend/index.php
+++ Frontend/index.php
@@ -1,5 +1,8 @@
 <?php
-
+  session_start();
+  	if($_SESSION["inloggad"]){
+		header("location:admin.php");
+	}
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +11,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./Styles/main.css">
-    <link rel="stylesheet" type="text/css" href="./Styles/kassa.css"> 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="./Styles/login.css"> 
     <title>Login</title>
